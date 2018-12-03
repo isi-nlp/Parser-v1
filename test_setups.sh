91b31b1d37337b99f85b413cd546462822f8c80b
ronaldahmed <roncardenasacosta@gmail.com>
2018-12-03 21:44:16 +0100

    conf

--- test_setups.sh
+++ test_setups.sh
@@ -3,15 +3,20 @@
 lang=$1
 gpu=$2
 
+echo "============================================================================"
+echo "$lang - guo.id.punk"
 CUDA_VISIBLE_DEVICES=$gpu python2 network.py --config_file=config/$lang-guo-id.cfg \
 --test --unk_pos=True --zero_wrd_emb=False \
-> saves/res.guo.id.punk
+> saves/res.$lang.guo.id.punk
 
-
+echo "============================================================================"
+echo "$lang - unk.id.punk"
 CUDA_VISIBLE_DEVICES=$gpu python2 network.py --config_file=config/$lang-guo-id.cfg \
 --test --unk_pos=True --zero_wrd_emb=True \
-> saves/res.unk.id.punk
+> saves/res.$lang.unk.id.punk
 
+echo "============================================================================"
+echo "$lang - unk.emb.punk"
 CUDA_VISIBLE_DEVICES=$gpu python2 network.py --config_file=config/$lang-unk-emb.cfg \
 --test --unk_pos=True --zero_wrd_emb=True \
-> saves/res.unk.emb.punk
+> saves/res.$lang.unk.emb.punk
