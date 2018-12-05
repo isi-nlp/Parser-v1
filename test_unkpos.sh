c6e33d8600b40ba69291dd68f4bcad1223b07c3e
ronaldahmed <roncardenasacosta@gmail.com>
2018-12-05 10:05:17 +0100

    conf

--- test_unkpos.sh
+++ test_unkpos.sh
@@ -44,20 +44,40 @@ tail -10 >> saves/res.$lang.unkpos
 
 
 echo "============================================================================"
-echo "$lang - unk.emb.tr=unkpos"
+echo "$lang - guo.emb.tr=unkpos"
 
-echo "$lang - unk.emb.gold" >> saves/res.$lang.unkpos
+echo "$lang - guo.emb.gold" >> saves/res.$lang.unkpos
 CUDA_VISIBLE_DEVICES=$gpu python2 network.py --config_file=config/$lang-unk-emb-unkpos.cfg \
---test --zero_wrd_emb=True --zero_test_pos=False | \
+--test --zero_wrd_emb=False --zero_test_pos=False | \
 tail -10 >> saves/res.$lang.unkpos
 
-echo "$lang - unk.emb.cipher" >> saves/res.$lang.unkpos
+echo "$lang - guo.emb.cipher" >> saves/res.$lang.unkpos
 CUDA_VISIBLE_DEVICES=$gpu python2 network.py --config_file=config/$lang-unk-emb-unkpos.cipher.cfg \
---test --zero_wrd_emb=True --zero_test_pos=False | \
+--test --zero_wrd_emb=False --zero_test_pos=False | \
 tail -10 >> saves/res.$lang.unkpos
 
-echo "$lang - unk.emb.unkpos" >> saves/res.$lang.unkpos
+echo "$lang - guo.emb.unkpos" >> saves/res.$lang.unkpos
 CUDA_VISIBLE_DEVICES=$gpu python2 network.py --config_file=config/$lang-unk-emb-unkpos.cfg \
---test --zero_wrd_emb=True --zero_test_pos=True | \
+--test --zero_wrd_emb=False --zero_test_pos=True | \
 tail -10 >> saves/res.$lang.unkpos
+
+
+
+# echo "============================================================================"
+# echo "$lang - unk.emb.tr=unkpos"
+
+# echo "$lang - unk.emb.gold" >> saves/res.$lang.unkpos
+# CUDA_VISIBLE_DEVICES=$gpu python2 network.py --config_file=config/$lang-unk-emb-unkpos.cfg \
+# --test --zero_wrd_emb=True --zero_test_pos=False | \
+# tail -10 >> saves/res.$lang.unkpos
+
+# echo "$lang - unk.emb.cipher" >> saves/res.$lang.unkpos
+# CUDA_VISIBLE_DEVICES=$gpu python2 network.py --config_file=config/$lang-unk-emb-unkpos.cipher.cfg \
+# --test --zero_wrd_emb=True --zero_test_pos=False | \
+# tail -10 >> saves/res.$lang.unkpos
+
+# echo "$lang - unk.emb.unkpos" >> saves/res.$lang.unkpos
+# CUDA_VISIBLE_DEVICES=$gpu python2 network.py --config_file=config/$lang-unk-emb-unkpos.cfg \
+# --test --zero_wrd_emb=True --zero_test_pos=True | \
+# tail -10 >> saves/res.$lang.unkpos
 
